59242f9802dd30dd62642bff9d8f3caef8709bd4
Bituhh <victor@oliveira.org.uk>
2019-06-15 21:56:09 +0100

    fully functioning pagination. ToDo: Add authetication and My Album module

--- src/app/shared/album.model.ts
+++ src/app/shared/album.model.ts
@@ -1,6 +1,9 @@
 import {ImageModel} from './image.model';
 
 export class AlbumModel {
+  itemPerPage = 12;
+  currentPage = 1;
+
   constructor(public name: string, public albumCover: ImageModel, public images: ImageModel[]) {
   }
 }
--- src/app/user-details/album-details/album-details.component.ts
+++ src/app/user-details/album-details/album-details.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../../shared/user.service';
 import {ActivatedRoute, Params} from '@angular/router';
 import {AlbumModel} from '../../shared/album.model';
+import {ImageModel} from '../../shared/image.model';
 
 @Component({
   selector: 'app-album-details',
@@ -10,9 +11,16 @@ import {AlbumModel} from '../../shared/album.model';
 })
 export class AlbumDetailsComponent implements OnInit {
   @Input() userIndex: number;
-  albumIndex: number;
+  private albumIndex: number;
+
   displayAlbum: AlbumModel;
-  viewMode = 'col-3';
+  displayImages: ImageModel[];
+  private displayImagesLimit = 12;
+  private viewMode = 'col-3';
+
+  currentPage = 1;
+  maximumPage: number;
+  pages = [];
 
   constructor(private route: ActivatedRoute, private userService: UserService) {
   }
@@ -23,12 +31,16 @@ export class AlbumDetailsComponent implements OnInit {
         this.userIndex = params.id as number;
         this.albumIndex = this.albumIndex ? this.albumIndex : 0;
         this.displayAlbum = this.userService.getAlbum(this.userIndex, this.albumIndex);
+        this.maximumPage = Math.ceil(this.displayAlbum.images.length / this.displayImagesLimit);
+        this.getPages(0);
       }
     );
     this.route.queryParams.subscribe(
       (params: Params) => {
         this.albumIndex = params.album ? params.album : 0;
         this.displayAlbum = this.userService.getAlbum(this.userIndex, this.albumIndex);
+        this.maximumPage = Math.ceil(this.displayAlbum.images.length / this.displayImagesLimit);
+        this.getPages(0);
       }
     );
   }
@@ -44,5 +56,46 @@ export class AlbumDetailsComponent implements OnInit {
       'col-md-4': this.viewMode === 'col-3',
       'col-md-3': this.viewMode === 'col-4'
     };
+  }
+
+  getPages(index) {
+    let c = this.pages[index];
+
+    // if '...' is selected pick next page.
+    if (c === '...') {
+      const previousIndex = this.pages.findIndex(p => {
+          return p === this.currentPage;
+        }
+      );
+      c = previousIndex > index ? (this.pages[index + 1] - 1) : (this.pages[index - 1] + 1);
+    }
+    const delta = 2;
+    const left = c - delta;
+    const right = c + delta + 1;
+    const range = [];
+    let l;
+
+    this.pages = [];
+
+    for (let i = 1; i <= this.maximumPage; i++) {
+      if (i === 1 || i === this.maximumPage || i >= left && i < right) {
+        range.push(i);
+      }
+    }
+
+    for (const i of range) {
+      if (l) {
+        if (i - l === 2) {
+          this.pages.push(l + 1);
+        } else if (i - l !== 1) {
+          this.pages.push('...');
+        }
+      }
+      this.pages.push(i);
+      l = i;
+    }
+
+    this.currentPage = c;
+    this.displayImages = this.displayAlbum.images.slice((c - 1) * this.displayImagesLimit, c * this.displayImagesLimit);
   }
 }
